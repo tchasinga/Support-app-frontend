099ad9d9ba2579783158cab5cc81155170695833
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-01 01:41:06 +0200

    New code is changes

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,37 @@
+
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+
+const handler = NextAuth({
+
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Credentials',
+      credentials: {
+        username: { label: "Email", type: "email", placeholder: "example@gmail.com" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        
+        const res =await fetch('http://localhost:2000/api/auth/signinuser', {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" }
+        })
+        const user = await res.json()
+  
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user
+        }
+        // Return null if user data could not be retrieved
+        return null
+      }
+    })
+  ]
+  
+})
+
+export { handler as GET, handler as POST }
