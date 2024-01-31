cea42beb08161709f1785f846a515e811d925985
Tchasinga jacques <tchasingajacques@gmail.com>
2024-01-31 21:44:46 +0200

    new code is added now

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -7,25 +7,25 @@ import Link from "next/link";
 export default function page() {
   return (
     <div className='max-w-4xl mx-auto flex flex-col justify-center w-full items-center py-40'>
-        <Text variant='xxLarge'>login page</Text>
+        <Text variant='xxLarge'>creating an account...</Text>
         
         <div className="">
             <form className='flex flex-col gap-4 pt-5'>
-                {/* <div>
-                <TextField type='text'  placeholder="Please enter your name here" />
-                </div> */}
+                <div>
+                <TextField type='text' className='w-96' placeholder="Please enter your name here" />
+                </div>
 
                 <div>
-                <TextField className='w-96' type='email' placeholder="Please enter your email" />
+                <TextField type='email' placeholder="Please enter your email" />
                 </div>
 
                 <div>
                 <TextField placeholder="Please enter your password" type="password" canRevealPassword revealPasswordAriaLabel="Show password"/>
                 </div>
-                <DefaultButton className='w-28' text="Login now"  allowDisabledFocus  />
+                <DefaultButton className='w-28' text="Sing up"  allowDisabledFocus  />
             </form>
         </div>
-        <Text className='pt-4' variant='medium'>Don&apos;t have an account? <Link className='text-green-500 font-normal' href='/register'>Register now</Link></Text>
+        <Text className='pt-4' variant='medium'>Have already an account? <Link className='text-green-500 font-normal' href='/login'>Login</Link></Text>
     </div>
   )
 }
