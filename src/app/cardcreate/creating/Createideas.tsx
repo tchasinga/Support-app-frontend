2d0511592aed29c05a4bba03ebf6e8724aed9d4d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-01 12:51:35 +0200

    New code is added now

--- src/app/cardcreate/creating/Createideas.tsx
+++ src/app/cardcreate/creating/Createideas.tsx
@@ -1,33 +1,70 @@
 
-"use client"
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 import { DefaultButton, Text , TextField } from "@fluentui/react";
 export default function Createideas() {
+    const [formData, setFormData] = useState({
+        author: "",
+        functionality: "",
+        email: "",
+        title: "",
+        description: "",
+      });
+
+
+    const handleCreateChanges = async (e : any) => {
+        e.preventDefault();
+        setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+        console.log(formData);
+      };
+
+        const handlerDatacreating = async (e: any) => {
+            if (!formData.author || !formData.functionality || !formData.email || !formData.title || !formData.description) {
+            return;
+            }
+            e.preventDefault();
+            let data;
+            try {
+            const response = await fetch(
+                "http://localhost:2000/api/cardidea/creatingideas",
+                {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(formData),
+                }
+            );
+            data = await response.json();
+            console.log(data);
+            } catch (error) {
+            console.log(error);
+            }
+        };
+
   return (
     <div className='flex flex-col justify-center items-center mt-36'>
         <Text variant='xLarge'>Creating ideas user </Text>
         <div className="">
-             <form className='mt-4 flex flex-col gap-2'>
+             <form className='mt-4 flex flex-col gap-2 flex-wrap w-full' onSubmit={handlerDatacreating}>
 
               <div className="flex flex-wrap gap-2">
 
               <div className="">
-                    <TextField className='w-96'  type='text' placeholder="Please enter your email"/>
+                    <TextField className='w-96'  id='author' type='text' placeholder="Author name" onChange={handleCreateChanges}/>
                 </div>
                 <div className="">
-                    <TextField className='w-96'  type='text' placeholder="Please enter your email"/>
+                    <TextField className='w-96' id='functionality'  type='text' placeholder="Please enter your functionality" onChange={handleCreateChanges}/>
                 </div>
               </div>
               <div className="border">
-                    <TextField className='w-full'  type='text' placeholder="Please enter your email"/>
+                    <TextField className='w-full'  id='email' type='text' placeholder="Please enter your email" onChange={handleCreateChanges}/>
                 </div>
 
                 <div className="border">
-                    <TextField className='w-full'  type='text' placeholder="Please enter your email"/>
+                    <TextField className='w-full'  id='title' type='text' placeholder="Please enter your title" onChange={handleCreateChanges}/>
                 </div>
 
                 <div className="border">
-                    <TextField className='w-full'  type='text' placeholder="Please enter your email"/>
+                <TextField id='description' type='text' multiline onChange={handleCreateChanges}/>
                 </div>
 
              </form>
